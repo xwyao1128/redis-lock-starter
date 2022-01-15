4141f2b45144251fc38623ce7fbf92eb223bc9a0
yaoxiangwen <yaoxiangwen@outlook.com>
2022-01-15 17:31:24 +0800

    initialization

---
+++ src/main/java/com/bupt/yxw/redislockstarter/RedisLockStarterApplication.java
@@ -0,0 +1,13 @@
+package com.bupt.yxw.redislockstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLockStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisLockStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bupt/yxw/redislockstarter/RedisLockStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bupt.yxw.redislockstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisLockStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
